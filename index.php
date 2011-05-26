28e9bad19ec12c4e5c55ac062eaed1c99e635c9f
Francesco Tassi <tassi.francesco@gmail.com>
2011-05-26 14:47:54 +0200

    integrato twig

--- index.php
+++ index.php
@@ -6,8 +6,17 @@ require_once __DIR__ . '/silex.phar';
 
 $app = new Application();
 
-$app->get('/', function(){
-  return 'Hello world';
+$app->register(new Silex\Extension\TwigExtension(), array(
+   'twig.path'  =>  __DIR__ . '/views' ,
+    'twig.class_path' =>  __DIR__ . '/vendor/twig/lib',
+));
+
+$app->before(function() use ($app){
+  $app['twig']->addGlobal('layout', $app['twig']->loadTemplate('layout.twig'));
+});
+
+$app->get('/', function() use ($app) {
+  return $app['twig']->render('index.twig');
 });
 
 $app->run();
